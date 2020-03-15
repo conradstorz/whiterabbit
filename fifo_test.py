ec4248c5fde0537cd654e7ac58c2c7051e4470ad
Conrad <conradstorz@gmail.com>
2020-03-14 17:53:19 -0700

    incr

--- fifo_test.py
+++ fifo_test.py
@@ -10,11 +10,13 @@ if os.path.exists(FIFO_PATH):
 
 if not os.path.exists(FIFO_PATH):
     os.mkfifo(FIFO_PATH)
-    my_fifo = open(FIFO_PATH, 'w')
-    print("my_fifo:", my_fifo)
+    with open('File.txt', 'a+') as file:
+        file.write("my_fifo:")
+    #my_fifo = open(FIFO_PATH, 'w')
+    #print("my_fifo:", my_fifo)
 
-pipe = Popen('/bin/date', shell=False, stdin=PIPE, stdout=my_fifo, stderr=PIPE)
-
-print(open(FIFO_PATH, 'r').readline())
+#pipe = Popen('/bin/date', shell=False, stdin=PIPE, stdout=my_fifo, stderr=PIPE)
+with open('File.txt', 'a+') as file:
+    print(file.readline())
 
 os.unlink(FIFO_PATH)
