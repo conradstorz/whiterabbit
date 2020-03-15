776e973dc6a474442f064738a895c58e202b18cf
Conrad <conradstorz@gmail.com>
2020-03-14 17:35:20 -0700

    incr

--- fifo_test.py
+++ fifo_test.py
@@ -10,7 +10,7 @@ if os.path.exists(FIFO_PATH):
 
 if not os.path.exists(FIFO_PATH):
     os.mkfifo(FIFO_PATH)
-    my_fifo = open(FIFO_PATH, 'w+')
+    my_fifo = open(FIFO_PATH, 'w')
     print("my_fifo:", my_fifo)
 
 pipe = Popen('/bin/date', shell=False, stdin=PIPE, stdout=my_fifo, stderr=PIPE)
