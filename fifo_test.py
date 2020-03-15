5ad6d609a2981244a46c09a0966aaa82e26728b4
Conrad <conradstorz@gmail.com>
2020-03-14 17:32:44 -0700

    updated print statements to py3

--- fifo_test.py
+++ fifo_test.py
@@ -11,10 +11,10 @@ if os.path.exists(FIFO_PATH):
 if not os.path.exists(FIFO_PATH):
     os.mkfifo(FIFO_PATH)
     my_fifo = open(FIFO_PATH, 'w+')
-    print "my_fifo:", my_fifo
+    print("my_fifo:", my_fifo)
 
 pipe = Popen('/bin/date', shell=False, stdin=PIPE, stdout=my_fifo, stderr=PIPE)
 
-print open(FIFO_PATH, 'r').readline()
+print(open(FIFO_PATH, 'r').readline())
 
 os.unlink(FIFO_PATH)
